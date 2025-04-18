3e4d0cd837073f124ab50fd684b05441f12d6fa8
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-04-18 12:45:26 -0500

    repository Producto

---
+++ src/main/java/com/example/comercial/repository/ProduuctoDAO.java
@@ -0,0 +1,11 @@
+package com.example.comercial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.comercial.model.Producto;
+
+@Repository
+public interface ProduuctoDAO extends JpaRepository<Producto, Long> {
+    
+}
